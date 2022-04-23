a095e3c60dbab106bb1c84488447a8505908061a
Reuben Shilling <chiefthunderfoot@gmail.com>
2022-04-22 19:32:45 -0500

    Implement LinearRing property

--- EpsgCoordinateSystems/Categories/NAD83_Delaware_ftUS.cs
+++ EpsgCoordinateSystems/Categories/NAD83_Delaware_ftUS.cs
@@ -1,4 +1,4 @@
-using DotSpatial.Projections;namespace EpsgCoordinateSystems.Categories
+using SharpKml.Base;using SharpKml.Dom;using DotSpatial.Projections;namespace EpsgCoordinateSystems.Categories
 {
     internal class NAD83_Delaware_ftUS : INad83StatePlaneUsFtCoordinateSystem
     {private const int _srid = 2235; public ProjectionInfo ProjectionInfo => ProjectionInfo.FromEpsgCode(_srid);
@@ -11,5 +11,42 @@ using DotSpatial.Projections;namespace EpsgCoordinateSystems.Categories
 
         public string EsriWkt =>
             "PROJCS[NAD83 / Delaware (ftUS),GEOGCS[GCS_North_American_1983,DATUM[D_North_American_1983,SPHEROID[GRS_1980,6378137,298.257222101]],PRIMEM[Greenwich,0],UNIT[Degree,0.017453292519943295]],PROJECTION[Transverse_Mercator],PARAMETER[latitude_of_origin,38],PARAMETER[central_meridian,-75.41666666666667],PARAMETER[scale_factor,0.999995],PARAMETER[false_easting,656166.667],PARAMETER[false_northing,0],UNIT[Foot_US,0.30480060960121924]]";
+public LinearRing Wgs84Boundary => new LinearRing{Coordinates = new CoordinateCollection(new[]{new Vector(-75.724592,38.8302858181819,0),
+new Vector(-75.710705,38.6496648181819,0),
+new Vector(-75.707069,38.5575948181819,0),
+new Vector(-75.698798,38.463184818182,0),
+new Vector(-75.3498409999999,38.4553188181818,0),
+new Vector(-75.09272,38.4505668181819,0),
+new Vector(-75.067925,38.4500718181818,0),
+new Vector(-75.04562300000001,38.4496038181818,0),
+new Vector(-75.08276600000001,38.7999288181819,0),
+new Vector(-75.190577,38.8087848181819,0),
+new Vector(-75.3075319999999,38.9460258181817,0),
+new Vector(-75.324488,39.0124998181819,0),
+new Vector(-75.397361,39.0731418181817,0),
+new Vector(-75.40212200000001,39.2577588181818,0),
+new Vector(-75.515216,39.3670548181819,0),
+new Vector(-75.58983499999989,39.4638858181818,0),
+new Vector(-75.562646,39.5668368181819,0),
+new Vector(-75.610373,39.6129078181819,0),
+new Vector(-75.489278,39.7148508181818,0),
+new Vector(-75.475976,39.7200798181818,0),
+new Vector(-75.474761,39.7418328181818,0),
+new Vector(-75.46038799999999,39.7633608181819,0),
+new Vector(-75.4276459999999,39.7782468181819,0),
+new Vector(-75.41175200000001,39.7897758181819,0),
+new Vector(-75.420464,39.7989828181818,0),
+new Vector(-75.469982,39.8265498181818,0),
+new Vector(-75.583445,39.840121818182,0),
+new Vector(-75.64398799999999,39.8383128181819,0),
+new Vector(-75.69477500000001,39.8204568181819,0),
+new Vector(-75.7455889999999,39.7749258181819,0),
+new Vector(-75.774929,39.7245528181819,0),
+new Vector(-75.791093,39.7238598181819,0),
+new Vector(-75.772355,39.3831198181819,0),
+new Vector(-75.764318,39.2959638181819,0),
+new Vector(-75.761312,39.2478678181819,0),
+new Vector(-75.752573,39.1416678181819,0),
+new Vector(-75.724592,38.8302858181819,0)})};
     }
 }
--- EpsgCoordinateSystems/Categories/NAD83_Utah_South_ftUS.cs
+++ EpsgCoordinateSystems/Categories/NAD83_Utah_South_ftUS.cs
@@ -1,4 +1,4 @@
-using DotSpatial.Projections;namespace EpsgCoordinateSystems.Categories
+using SharpKml.Base;using SharpKml.Dom;using DotSpatial.Projections;namespace EpsgCoordinateSystems.Categories
 {
     internal class NAD83_Utah_South_ftUS : INad83StatePlaneUsFtCoordinateSystem
     {private const int _srid = 3567; public ProjectionInfo ProjectionInfo => ProjectionInfo.FromEpsgCode(_srid);
@@ -11,5 +11,36 @@ using DotSpatial.Projections;namespace EpsgCoordinateSystems.Categories
 
         public string EsriWkt =>
             "PROJCS[NAD83 / Utah South (ftUS),GEOGCS[GCS_North_American_1983,DATUM[D_North_American_1983,SPHEROID[GRS_1980,6378137,298.257222101]],PRIMEM[Greenwich,0],UNIT[Degree,0.017453292519943295]],PROJECTION[Lambert_Conformal_Conic],PARAMETER[standard_parallel_1,38.35],PARAMETER[standard_parallel_2,37.21666666666667],PARAMETER[latitude_of_origin,36.66666666666666],PARAMETER[central_meridian,-111.5],PARAMETER[false_easting,1640416.6667],PARAMETER[false_northing,9842500.000000002],UNIT[Foot_US,0.30480060960121924]]";
+public LinearRing Wgs84Boundary => new LinearRing{Coordinates = new CoordinateCollection(new[]{new Vector(-109.045598,37.6308198181818,0),
+new Vector(-109.043213,37.8874188181819,0),
+new Vector(-109.043465,38.1529368181818,0),
+new Vector(-109.055867,38.2449168181819,0),
+new Vector(-109.053941,38.4946578181819,0),
+new Vector(-110.039702,38.4980148181818,0),
+new Vector(-111.303662,38.4998508181819,0),
+new Vector(-111.304274,38.5071498181818,0),
+new Vector(-111.740027,38.5092828181819,0),
+new Vector(-111.768692,38.4955218181818,0),
+new Vector(-112.067159,38.4964938181819,0),
+new Vector(-112.066376,38.5082568181819,0),
+new Vector(-112.513973,38.5067538181819,0),
+new Vector(-112.517546,38.5731288181818,0),
+new Vector(-113.127773,38.5765578181819,0),
+new Vector(-113.127449,38.5715538181819,0),
+new Vector(-113.563562,38.5790238181818,0),
+new Vector(-114.045089,38.5710948181818,0),
+new Vector(-114.047276,38.1376548181818,0),
+new Vector(-114.047267,37.5984828181818,0),
+new Vector(-114.043937,36.9965358181818,0),
+new Vector(-112.899992,36.9962298181819,0),
+new Vector(-112.542521,36.9979938181818,0),
+new Vector(-112.237259,36.9954918181819,0),
+new Vector(-111.356168,37.0017108181819,0),
+new Vector(-110.740064,37.0024938181818,0),
+new Vector(-110.484095,37.0039248181819,0),
+new Vector(-110.452244,36.9917478181819,0),
+new Vector(-109.997069,36.9920628181819,0),
+new Vector(-109.048478,36.9966438181818,0),
+new Vector(-109.045598,37.6308198181818,0)})};
     }
 }
